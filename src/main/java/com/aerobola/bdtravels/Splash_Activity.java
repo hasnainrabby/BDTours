84f995f90cab5663693925f58529501bb341604e
rijvi <rijvizaman8@gmail.com>
2018-10-03 16:35:46 +0600

    New Screen

--- src/main/java/com/aerobola/bdtravels/Splash_Activity.java
+++ src/main/java/com/aerobola/bdtravels/Splash_Activity.java
@@ -30,7 +30,7 @@ public class Splash_Activity extends AppCompatActivity {
             @Override
             public void run() {
 
-                Intent intent=new Intent(Splash_Activity.this,MainActivity.class);
+                Intent intent = new Intent(Splash_Activity.this, FirstPage.class);
                 startActivity(intent);
                 finish();
 

5e9cb20c96145f75c553b5f3ddbf57176775f72d
rijvi <rijvizaman8@gmail.com>
2018-10-03 14:58:22 +0600

    Initial commit

---
+++ src/main/java/com/aerobola/bdtravels/Splash_Activity.java
@@ -0,0 +1,41 @@
+package com.aerobola.bdtravels;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                splash2();
+            }
+        },3500);
+    }
+
+
+    public void splash2(){
+        setContentView(R.layout.splash_screen_2);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent=new Intent(Splash_Activity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },5000);
+
+    }
+}
